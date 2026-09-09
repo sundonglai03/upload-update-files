fd993c30b504e39d7b4735ba0080bfcd6d95aa32
sundonglai03 <sundonglai03@gmail.com>
2026-09-09 22:12:22 +0800

    删除多余配置

--- complete-kotlin/src/main/kotlin/com/example/uploadingfiles/storage/StorageFileNotFoundException.kt
+++
@@ -1,4 +0,0 @@
-package com.example.uploadingfiles.storage
-
-class StorageFileNotFoundException(message: String, cause: Throwable? = null) :
-  StorageException(message, cause)
--- initial-kotlin/src/main/kotlin/com/example/uploadingfiles/storage/StorageFileNotFoundException.kt
+++
@@ -1,8 +0,0 @@
-package com.example.uploadingfiles.storage
-
-class StorageFileNotFoundException : StorageException {
-
-  constructor(message: String) : super(message)
-
-  constructor(message: String, cause: Throwable) : super(message, cause)
-}
